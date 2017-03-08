df3f11613caca9125ef5fc6ae619eb2692168132
just2click <just2click@outlook.com>
2017-03-09 01:27:54 +0200

    Fixed

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 function createButton ($container, msg) {
 
 	var $btn = document.createElement('button');
+
 	$btn.style.height = '100px';
 	$btn.style.width = '100px';
 	$btn.textContent = msg;
 
-	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
-	$btn.onmouseenter = function () {
-		var posWidth = getRandomInt(0, w - 100),
-			posHeight = getRandomInt(0, h - 100);
-
-		this.style.transition = 'all 0.5s ease-out';
-		this.style.transform = 'translate(' + posWidth + 'px,' + posHeight + 'px)';
-	};
+	// var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	// var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 	$container.appendChild($btn);
 }
 
-var $btn = createButton(document.querySelector('.container'), 'Click Me!');
+function buttonMove(e, $btn) {
+		var posWidth = (e.pageX - 50),
+			posHeight = (e.pageY - 50);
+
+		$btn.style.transition = 'all 0.5s ease-out';
+		$btn.style.transform = 'translate(' + posWidth + 'px,' + posHeight + 'px)';
+}
+
+createButton(document.querySelector('body'), 'Click Me!');
+
+window.onmousemove = function (e) {
+	buttonMove(e, document.querySelector('button'));
+};
+
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
