aa5edaffc9a568ed5906fd518c53d6864a1419ce
just2click <just2click@outlook.com>
2017-03-08 20:34:27 +0200

    Fixed code

--- main.js
+++ main.js
@@ -5,27 +5,21 @@ function createButton ($container, msg) {
 	$btn.style.width = '100px';
 	$btn.textContent = msg;
 
-	$container.appendChild($btn);
-
-	return $btn;
-}
-
-function moveButton($btn) {
 	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-	var posWidth = getRandomInt(0, w - 100),
-		posHeight = getRandomInt(0, h - 100);
+	$btn.onmouseenter = function () {
+		var posWidth = getRandomInt(0, w - 100),
+			posHeight = getRandomInt(0, h - 100);
 
-	$btn.style.transition = 'all 0.5s ease-out';
-	$btn.style.transform = 'translate(' + posWidth + 'px,' + posHeight + 'px)';
+		this.style.transition = 'all 0.5s ease-out';
+		this.style.transform = 'translate(' + posWidth + 'px,' + posHeight + 'px)';
+	};
+
+	$container.appendChild($btn);
 }
 
 var $btn = createButton(document.querySelector('.container'), 'Click Me!');
-
-$btn.addEventListener('mouseenter', function () {
-	moveButton($btn);
-});
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
